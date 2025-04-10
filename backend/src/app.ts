16b89bb4aeeacf2b9738570f70da73200a74b200
Ondrej Slejtr <o.slejtr@gmail.com>
2025-04-10 21:32:33 +0200

    feat: trust Render's proxy with encryption

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -11,6 +11,7 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.enable("trust proxy");
 
 const FRONTEND_URL = process.env.FRONTEND_URL;
 
