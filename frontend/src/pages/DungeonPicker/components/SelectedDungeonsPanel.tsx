4ef95809d5ab68fbe7ce122b742f163a0a3f6059
Ondrej Slejtr <o.slejtr@gmail.com>
2025-04-12 19:51:24 +0200

    feat: submission button

--- frontend/src/pages/DungeonPicker/components/SelectedDungeonsPanel.tsx
+++ frontend/src/pages/DungeonPicker/components/SelectedDungeonsPanel.tsx
@@ -5,17 +5,30 @@ interface SelectedDungeonsPanelProps {
     selectedDungeons: Dungeon[];
     maxSelection: number;
     onRemoveDungeon: (dungeonId: number) => void;
+    onSubmitSelection: () => void; // New prop for handling submission
 }
 
-const SelectedDungeonsPanel = ({ selectedDungeons, maxSelection, onRemoveDungeon }: SelectedDungeonsPanelProps) => {
+const SelectedDungeonsPanel = ({
+    selectedDungeons,
+    maxSelection,
+    onRemoveDungeon,
+    onSubmitSelection,
+}: SelectedDungeonsPanelProps) => {
     const getImageUrl = (name: string, expansion: string) => {
         return new URL(`../../../assets/dungeons/${expansion}/${name}.png`, import.meta.url).href;
     };
+
     return (
-        <div className={styles.dropPanel}>
-            <h2 className={selectedDungeons.length === maxSelection ? styles.full : ""}>
-                Your Selection ({selectedDungeons.length}/{maxSelection})
-            </h2>
+        <div className={styles.selectionPanel}>
+            {selectedDungeons.length === maxSelection ? (
+                <button className={styles.submitButton} onClick={onSubmitSelection}>
+                    Submit Your Selection
+                </button>
+            ) : (
+                <h2>
+                    Your Selection ({selectedDungeons.length}/{maxSelection})
+                </h2>
+            )}
             {selectedDungeons.length === 0 ? (
                 <p className={styles.placeholder}>No dungeons selected</p>
             ) : (
